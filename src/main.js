6fe1ef6548247b7584a05139c94eeeec4c5f4e8e
Nicolas Vargas <nicolas.vargas1012@gmail.com>
2021-11-15 23:41:51 -0500

    connection to bd

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import app from './app';
+import dbConnection from './db/config';
 require('dotenv').config();
 
 const port = process.env.PORT;
 
 const main = async() => {
+	await dbConnection();
 	await app.listen(port)
 	console.log('server running on port', port);
 }
